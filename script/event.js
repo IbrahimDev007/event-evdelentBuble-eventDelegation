0be6eef12cc2b3ba2faf34473f1d5e12fff7c76c
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-02-11 21:10:22 +0600

    event buble event declare and face problem

---
+++ script/event.js
@@ -0,0 +1,42 @@
+console.log("event js added");
+//onclick event handaler
+ function func(){
+    console.log("you succefully create onclick event");
+}
+
+//event handaler wiith event listner
+
+    document.getElementById("simple").addEventListener('mouseover',function () {
+     console.log("hello event")})
+
+// event handaler  append  item 
+
+let parent_item = document.getElementById("section-3");
+document.getElementById("submit").addEventListener('click', function() {
+  let textarea_value = document.getElementById('textarea').value;
+  let child_item = document.createElement("p");
+  child_item.innerText = textarea_value;
+  parent_item.appendChild(child_item);
+  document.getElementById("textarea").value = "";
+});
+//face problem  append child not accept text
+//  //event delegation
+
+    document.getElementById("delete").addEventListener('click',function (event) {
+        document.getElementById("parent").addEventListener
+    })
+    
+ 
+
+ //disabled button
+
+ document.getElementById("email-text").addEventListener("keyup", function () {
+    let email = document.getElementById("email-text").value.toLowerCase();
+    if (email === "email") {
+      document.getElementById("email").removeAttribute("disabled");
+    } else {
+      document.getElementById("email").setAttribute("disabled", true);
+    }
+  });
+
+  //get face problem remove atribut
